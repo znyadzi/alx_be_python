29ff84d41332bd7dde8bca09d34f2545a3ef9ae6
znyadzi <125977854+znyadzi@users.noreply.github.com>
2024-06-19 21:33:20 +0000

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
