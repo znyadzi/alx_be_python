9a3b83ce23a7a5d8ef4cd1db38446c455e2a2a57
znyadzi <125977854+znyadzi@users.noreply.github.com>
2024-06-19 22:09:31 +0000

    created python files to run scripts

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,7 @@ principal = 1000
 rate = 0.05
 time = 3
 interest = (principal * rate * time)
-
-print(f"The simple interest is: {interest}")
+s *= principal 
+s *= rate 
+s *= time
+print(f"The simple interest is: {s}")
