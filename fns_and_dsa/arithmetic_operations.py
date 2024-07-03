e5ca3797b4463679eb0a44de42665b23eada14a0
znyadzi <125977854+znyadzi@users.noreply.github.com>
2024-07-03 12:53:44 +0000

    WEEK 5
    0. Arithmetic Operations Function update 2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         result = str(num1 * num2)
     elif operation == "divide":
-        if result != 0:
+        if num2 != 0:
             result = str(num1 / num2)
         else: result = "Division by zero is not allowed"
         
