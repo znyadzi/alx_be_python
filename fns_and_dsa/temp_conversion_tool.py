20bff03d5403e839847a28f92a8b2343ff090992
znyadzi <125977854+znyadzi@users.noreply.github.com>
2024-07-05 08:53:59 +0000

    UPDATE Still for chat GPT I did some small changes
    this is the update chat gpt gave but have been slightly edited

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Constants for conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     # Conversion formula: C = (F - 32) * (5/9)
