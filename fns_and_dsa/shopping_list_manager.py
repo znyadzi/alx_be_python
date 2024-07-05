c6e297615696108b4d90c0cd7f00d2f67660784f
znyadzi <125977854+znyadzi@users.noreply.github.com>
2024-07-05 05:37:47 +0000

    Create shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,31 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            shopping_list.append(input("Enter Item name"))
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            shopping_list.remove(input("Enter an item name to Remove it"))
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+main()
