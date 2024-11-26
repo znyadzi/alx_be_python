364fefa2bf3310c0a09773f2ddf250c173f7812e
znyadzi <125977854+znyadzi@users.noreply.github.com>
2024-11-26 17:02:52 +0000

    Update oop_concept.py

--- oop/oop_concept.py
+++ oop/oop_concept.py
@@ -24,6 +24,25 @@ class Car:
     def print_info(self):
         print("Car Manufacturer is ", self.manufacturer)
         print("The build date is ", build_date)
+    
+    #Another example
+    class Dog:
+    voice = "Woof"
+
+    def bark(self):
+        print(self.voice)
 """
 
-# 4 
+# 4 Updating variable values withn a class
+"""
+class Point:
+    x = 0
+    y = 0
+    
+    def update_x(self, new_x):
+        self.x = new_x
+    def update_y(self, new_y):
+        self.y = new_y
+"""
+
+# 5 
