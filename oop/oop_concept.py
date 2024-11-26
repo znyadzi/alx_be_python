c037e1d12e6b692c3cc77bd86f562965b8dd95a7
znyadzi <125977854+znyadzi@users.noreply.github.com>
2024-11-26 11:23:31 +0000

    still working on the OOP concept

---
+++ oop/oop_concept.py
@@ -0,0 +1,29 @@
+# 1 Declaring a class
+"""
+class Sport:
+    type = "Golf"
+"""
+
+# 2 Calling variables of a class
+
+"""
+class Person:
+    name = "Lord"
+
+lord = Person()
+print(lord.name)
+"""
+
+# 3 Methods in classes
+"""
+class Car:
+    manufacturer = "Mercedes"
+    build_date = 2022
+
+    #This function prints the manufacturer and the build year.
+    def print_info(self):
+        print("Car Manufacturer is ", self.manufacturer)
+        print("The build date is ", build_date)
+"""
+
+# 4 
